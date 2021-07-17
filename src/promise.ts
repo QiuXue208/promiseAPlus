89f48bfff29ee437392d4bb48f0db8c7c7f4e618
Charles <1309571292@qq.com>
2021-07-17 12:01:46 +0800

    promise代码优化

--- src/promise.ts
+++ src/promise.ts
@@ -1,15 +1,21 @@
 class Promise2 {
   onFulfilled = null
   onRejected = null
+  resolve() {
+    setTimeout(() => {
+      this.onFulfilled()
+    }, 0)
+  }
+  reject() {
+    setTimeout(() => {
+      this.onRejected()
+    }, 0)
+  }
   constructor(fn) {
     if (typeof fn !== 'function') {
       throw new Error('new Promise()必须接受一个函数')
     }
-    fn(() => {
-      setTimeout(() => {
-        this.onFulfilled()
-      }, 0)
-    }, () => {})
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(onFulfilled, onRejected) {
     this.onFulfilled = onFulfilled
